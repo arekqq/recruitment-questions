5b95e24db7657ded04a239268fefbd6b96fe74a3
Arkadiusz <arek.rogacki@gmail.com>
2024-09-25 10:30:53 +0200

    backend draft

---
+++ apps/backend/src/main/java/com/rq/RecrutimentQuestions/RecrutimentQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.rq.RecrutimentQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutimentQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutimentQuestionsApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/com/rq/RecrutimentQuestions/RecrutimentQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rq.RecrutimentQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutimentQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
